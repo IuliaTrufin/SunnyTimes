48c3eb3d63e5fdeeb913869879a8a15df526ec13
Lucian <raducanu.lucian93@gmail.com>
2020-03-31 21:50:39 +0300

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
-import { NewsComponent } from './news/news/news.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NewsComponent } from "./news/news/news.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BusinessComponent } from "./business/business.component";
 
-
-const routes: Routes = [{path:"business", pathMatch:"full", component:BusinessComponent},
-{path:"life", pathMatch:"full", component:LifeComponent},
-{path:"sports", pathMatch:"full", component:SportsComponent},
-{path:"national", pathMatch:"full", component:NationalComponent},
-{path:"international", pathMatch:"full", component:InternationalComponent},
-{path:"news", pathMatch:"full", component:NewsComponent},
-{path:"**", pathMatch:"full", redirectTo:"news"}];
+const routes: Routes = [
+  { path: "business", pathMatch: "full", component: BusinessComponent },
+  // {path:"life", pathMatch:"full", component:LifeComponent},
+  // {path:"sports", pathMatch:"full", component:SportsComponent},
+  // {path:"national", pathMatch:"full", component:NationalComponent},
+  // {path:"international", pathMatch:"full", component:InternationalComponent},
+  { path: "news", pathMatch: "full", component: NewsComponent },
+  { path: "**", pathMatch: "full", redirectTo: "news" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
